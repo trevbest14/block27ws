316e5cbd8986990f2a3845d3640e8abb62d26c9d
trevbest14 <150987625+trevbest14@users.noreply.github.com>
2024-04-04 23:39:53 -0700

    Add files via upload

---
+++ SignUpForm.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-unreachable */
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+import React from 'react'; 
+import { useState } from 'react';
+
+
+export default function SignUpForm() {
+    return <h2>Sign Up!</h2>;
+    <form>
+  <label>
+    Username: <input />
+  </label>
+  <label>
+    Password: <input />
+  </label>
+  <button>Submit</button>
+</form>
+  }
+ 
+
+  useState 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+  
